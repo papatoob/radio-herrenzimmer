18d1f60cf38f0437c9abbc3df1d102eaa8b28012
Onkel Toob <info@tobias-reinhardt.de>
2017-09-03 22:26:51 +0200

    edit Intro: Erste Version der Detailanimation

--- radio-herrenzimmer/src/app/app.module.ts
+++ radio-herrenzimmer/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { PlatformService } from './service/platform.service';
@@ -18,7 +19,8 @@ import { IntroComponent } from './component/intro/intro.component';
     IntroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [
     PlatformService,
--- radio-herrenzimmer/src/app/component/intro/intro.component.ts
+++ radio-herrenzimmer/src/app/component/intro/intro.component.ts
@@ -1,22 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  animations: [
+    trigger('detailsState', [
+      state('showDetails', style({
+        opacity: 1,
+        display: 'block'
+      })),
+      state('hideDetails', style({
+        opacity: 0,
+        display: 'none'
+      })),
+      transition('showDetails => hideDetails', animate('1000ms ease-out')),
+      transition('hideDetails => showDetails', animate('1000ms ease-in'))
+    ]),
+    trigger('detailsLinkState', [
+      state('hideDetails', style({
+        opacity: 1,
+        display: 'block'
+      })),
+      state('showDetails', style({
+        opacity: 0,
+        display: 'none'
+      })),
+      transition('hideDetails => showDetails', animate('1000ms ease-out')),
+      transition('showDetails => hideDetails', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class IntroComponent implements OnInit {
 
   // Gibt an, ob die lange Version des Intros angezeigt werden soll
-  showMore: boolean;
+  showDetails: boolean;
 
   // Erstellt ein neues Objekt vom Typ IntroComponent
   constructor() {
-    this.showMore = false;
+    this.showDetails = false;
+  }
+
+  // Gibt den aktuellen Status der Detailinformationen zurück
+  get currentDetailsState() {
+    return this.showDetails ? 'showDetails' : 'hideDetails'
+  }
+
+  // Steuert das Umschalten der Sichtbarkeit der Intro-Details
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
   }
 
   // Wird beim Initialisieren der Komponente ausgeführt
   ngOnInit() {
   }
-
 }
